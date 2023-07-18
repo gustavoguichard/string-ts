c27e52e78048d34d75423ade608b327806eb3fbc
Landon Yarrington <jly.dev.36963@gmail.com>
2023-07-18 06:59:44 -0600

    fix: screaming snake case

--- src/key-casing.test.ts
+++ src/key-casing.test.ts
@@ -106,6 +106,18 @@ describe('key transformation', () => {
     type test = Expect<Equal<typeof result, typeof expected>>
   })
 
+  test('deepCamelKeys on object using screaming snake case', () => {
+    const obj = {
+      NODE_ENV: 'development',
+    }
+    const expected = {
+      nodeEnv: 'development',
+    }
+    const result = subject.deepCamelKeys(obj)
+    expect(result).toEqual(expected)
+    type test = Expect<Equal<typeof result, typeof expected>>
+  })
+
   test('deepSnakeKeys', () => {
     const expected = {
       some: { deep_nested: { value: true } },
