738811abd0440972ed4435a861f246cf13346a37
Guga Guichard <gustavoguichard@gmail.com>
2023-10-05 10:21:05 -0300

    feat: Add strongly-typed padEnd and padStart alternatives (#31)

--- src/math.ts
+++ src/math.ts
@@ -7,9 +7,9 @@ namespace Math {
     B extends number,
   > = TupleOf<A> extends [...infer U, ...TupleOf<B>] ? U['length'] : 0
 
-  export type IsPositive<T extends number> = `${T}` extends `-${number}`
-    ? false
-    : true
+  export type IsNegative<T extends number> = `${T}` extends `-${number}`
+    ? true
+    : false
 
   export type Abs<T extends number> =
     `${T}` extends `-${infer U extends number}` ? U : T
@@ -17,7 +17,7 @@ namespace Math {
   export type GetPositiveIndex<
     T extends string,
     I extends number,
-  > = IsPositive<I> extends true ? I : Subtract<Length<T>, Abs<I>>
+  > = IsNegative<I> extends false ? I : Subtract<Length<T>, Abs<I>>
 }
 
 export type { Math }
