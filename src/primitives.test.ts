53ff09b0d9146860938be2fe6c7915c0ad0313d7
Landon Yarrington <jly.dev.36963@gmail.com>
2023-10-04 21:32:26 -0600

    Add endsWith

--- src/primitives.test.ts
+++ src/primitives.test.ts
@@ -211,6 +211,52 @@ describe('primitives', () => {
     })
   })
 
+  describe('endsWith', () => {
+    describe('without offset', () => {
+      test('should return true when text ends with search', () => {
+        const result = subject.endsWith('abc', 'c')
+        expect(result).toEqual(true)
+        type test = Expect<Equal<typeof result, true>>
+      })
+      test('should return false when text does not end with search', () => {
+        const result = subject.endsWith('abc', 'b')
+        expect(result).toEqual(false)
+        type test = Expect<Equal<typeof result, false>>
+      })
+    })
+
+    describe('with offset', () => {
+      test('should return true when offset text ends with search', () => {
+        const result = subject.endsWith('abc', 'b', 2)
+        expect(result).toEqual(true)
+        type test = Expect<Equal<typeof result, true>>
+      })
+      test('should return true when offset text ends with search (multi-char)', () => {
+        const result = subject.endsWith('abc', 'bc', 3)
+        expect(result).toEqual(true)
+        type test = Expect<Equal<typeof result, true>>
+      })
+      test('should return false when offset string does not end with search', () => {
+        const result = subject.endsWith('abc', 'c', 1)
+        expect(result).toEqual(false)
+        type test = Expect<Equal<typeof result, false>>
+      })
+    })
+
+    describe('with bad offset', () => {
+      test('should return false when the offset is negative', () => {
+        const result = subject.endsWith('abc', 'a', -1)
+        expect(result).toEqual(false)
+        type test = Expect<Equal<typeof result, false>>
+      })
+      test('should return true when the end matches and offset is greater than text length', () => {
+        const result = subject.endsWith('abc', 'c', 10)
+        expect(result).toEqual(true)
+        type test = Expect<Equal<typeof result, true>>
+      })
+    })
+  })
+
   test('trimStart', () => {
     test('should trim the start of a string at both type level and runtime level', () => {
       const data = ' some nice string '
--- src/primitives.ts
+++ src/primitives.ts
@@ -255,6 +255,40 @@ function startsWith<T extends string, S extends string, P extends number = 0>(
 }
 
 /**
+ * Checks if a string ends with another string.
+ * T: The string to check.
+ * S: The string to check against.
+ * P: The position the search should end.
+ */
+type EndsWith<
+  T extends string,
+  S extends string,
+  P extends number = Length<T>,
+> = Math.IsPositive<P> extends true
+  ? P extends Length<T>
+    ? S extends Slice<T, Math.Subtract<Length<T>, Length<S>>, Length<T>>
+      ? true
+      : false
+    : EndsWith<Slice<T, 0, P>, S, Length<T>> // P !== T.length, slice
+  : false // P is negative, false
+
+/**
+ * A strongly-typed version of `String.prototype.endsWith`.
+ * @param text the string to search.
+ * @param search the string to search with.
+ * @param position the index the search should end at.
+ * @returns boolean, whether or not the text string ends with the search string.
+ * @example endsWith('abc', 'c') // true
+ */
+function endsWith<
+  T extends string,
+  S extends string,
+  P extends number = Length<T>,
+>(text: T, search: S, position = text.length as P) {
+  return text.endsWith(search, position) as EndsWith<T, S, P>
+}
+
+/**
  * Trims all whitespaces at the start of a string.
  * T: The string to trim.
  */
@@ -305,6 +339,7 @@ function trim<T extends string>(str: T) {
 export type {
   CharAt,
   Concat,
+  EndsWith,
   Join,
   Length,
   Replace,
@@ -319,6 +354,7 @@ export type {
 export {
   charAt,
   concat,
+  endsWith,
   join,
   length,
   replace,
