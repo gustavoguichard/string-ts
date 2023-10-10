5bd61fc563786f63ada89fdb2641e0c9eb65623d
Guga Guichard <gustavoguichard@gmail.com>
2023-10-10 16:36:30 -0300

    test: Improve coverage

--- src/utils/object-keys/deep-transform-keys.test.ts
+++ src/utils/object-keys/deep-transform-keys.test.ts
@@ -3,12 +3,12 @@ import { deepTransformKeys } from './deep-transform-keys.js'
 describe('deepTransformKeys', () => {
   test('should deeply transform the keys of an object', () => {
     const expected = {
-      SOME: { 'DEEP-NESTED': { VALUE: true } },
+      SOME: [{ 'DEEP-NESTED': { VALUE: true } }],
       'OTHER-VALUE': true,
     }
     const result = deepTransformKeys(
       {
-        some: { 'deep-nested': { value: true } },
+        some: [{ 'deep-nested': { value: true } }],
         'other-value': true,
       },
       (key) => key.toUpperCase(),
